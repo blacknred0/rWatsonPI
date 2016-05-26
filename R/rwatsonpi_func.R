c8d5bd43886bf53e17f270231535b6b37077e854
Irving Duran <irving.duran@gmail.com>
2016-05-26 07:51:41 -0500

    RELEASE 1.8 - Major enhancements
    -Updated documentation to align with API defaults
    -Cleanup bunch of trailing spaces
    -Reduce system sleep time to something more manageable
    -Func fmtJSON() to remove new warnings being added by service
    -New build now includes jsonlite instead of rjson
    -Func selMeaningfulRecs() now allows user to select variables to keep
    -Func exportPI() allows user to name file for export

--- R/rwatsonpi_func.R
+++ R/rwatsonpi_func.R
@@ -1,4 +1,4 @@
-# Copyright 2015 IBM Corp. All Rights Reserved.
+# Copyright 2016 IBM Corp. All Rights Reserved.
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -23,7 +23,7 @@ pkgHC <- function(){
 	pkgs <- c("plyr", "RCurl", "jsonlite", "textcat") #update this section with the required packages
 	npkgs <- pkgs[!(pkgs %in% installed.packages()[,"Package"])] #check library if package is not installed
 	if(length(npkgs)) install.packages(npkgs, dependencies=TRUE, repos="http://cran.us.r-project.org") #if it is not installed, then install package with dependencies
-	
+
 	lapply(pkgs, require, character.only=T) #load require packages once they are all installed
 }
 
@@ -40,7 +40,7 @@ startPI <- function(x){
 		cat("\n Please wait for about 5-10 seconds and don't forget to run stopPI() function when you are done with your batches. \n")
 		# Start PI server and don't wait for anything to finish
 		system(paste("C:/Windows/system32/cmd.exe /c cd ", x, " & node app.js", sep=""), wait=FALSE)
-	}
+		}
 	if(Sys.info()['sysname']=="Linux"){
 		cat("\n Starting Node.js...")
 		cat("\n Please wait for about 5-10 seconds and don't forget to run stopPI() function when you are done with your batches. \n")
@@ -80,18 +80,18 @@ stopPI <- function(){
 clnTxt <- function(x) {
 	x <- gsub("[^0-9A-Za-z ]", " ", x) # only leaves letters and numbers
 	x <- gsub("^ *|(?<= ) | *$", "", x, perl=TRUE) # remove multiple and trailing spaces
-	#In internationalization, CJK is a collective term for the Chinese, Japanese, and Korean (CJK unicode). 
+	#In internationalization, CJK is a collective term for the Chinese, Japanese, and Korean (CJK unicode).
 	#This convert character vector between encoding by using system encoding for internationalization.  If the character doesn't exist on current encoding, then it will be removed.
 	iconv(x, "UTF-8", "ASCII", sub="")
-	
+
 	return(x)
 }
 
 #' Select meaningful records
 #'
-#' This clean up will allow PI to run smooth where records that might not meet the criteria below will be removed: 
-#' The number of words X is less than the minimum number of words required for analysis: 100 AND 
-#' The input you provided matched X words from our lexicon, we require 70 matching words to calculate characteristics with any confidence AND 
+#' This clean up will allow PI to run smooth where records that might not meet the criteria below will be removed:
+#' The number of words X is less than the minimum number of words required for analysis: 100 AND
+#' The input you provided matched X words from our lexicon, we require 70 matching words to calculate characteristics with any confidence AND
 #' Too many words .... greater than 10k.  These could be run manually in the tool instead of using RCURL as there is a max limitation  AND
 #' The records need to be in English in order to fully be process through Watson PI.
 #' This will produce two new vars, df.sel and df.notsel, where df.sel will be the records that meet the criteria and df.notsel would be the records that did not meet criteria.  At this point the user should be able to do a write.table() and export those records and do it manually where applicable.
@@ -118,7 +118,7 @@ selMeaningfulRecs <- function(x, txt, keep) {
 			df.notsel <<- xns[, keep] # only select needed columns
 		}
 	}
-	
+
 	cat("\n New data frame got created with the following variable name -> df.sel.  \n For the records not selected, they will be stored in data frame -> df.notsel.")
 	cat("\n You can run a write.table() to export those variables and you might be able to run them manually against the PI server. \n")
 }
@@ -135,22 +135,22 @@ selMeaningfulRecs <- function(x, txt, keep) {
 #' new.x2 <- getPI(url="https://www.example.com", x=data$field, dump=NULL, ssl=TRUE) # store results into new var x2, but information is encrypted with server
 getPI <- function(url, x, dump, ssl=FALSE){
 	dump = NULL;
-	
+
 	if(ssl==FALSE & substring(url, 0, 5)=="https"){
 		cat("\n Please run with ssl=TRUE to ensure that you get your results back. \n")
 	}
 	else if(ssl==FALSE){
 		if(url.exists(url)){
 			for (i in 1:length(x)){
-				dump[i] <- postForm(url, 
-									text = x[i], 
+				dump[i] <- postForm(url,
+									text = x[i],
 									button = "Analyze",
-									httpheader=c('Host'=paste("\"", url, "\"", sep=""), 'User-Agent'="Mozilla/5.0 (Windows NT 6.1; WOW64; rv:31.0) Gecko/20100101 Firefox/31.0", 'Accept'="application/json, text/javascript, */*; q=0.01", 'Accept-Language'="en-US,en;q=0.5", 'Accept-Encoding'="gzip, deflate", 'Content-Type'="application/x-www-form-urlencoded; charset=UTF-8", 'X-Requested-With'="XMLHttpRequest", 'Referer'=paste("\"http://", url, "\"", sep="")), 
+									httpheader=c('Host'=paste("\"", url, "\"", sep=""), 'User-Agent'="Mozilla/5.0 (Windows NT 6.1; WOW64; rv:31.0) Gecko/20100101 Firefox/31.0", 'Accept'="application/json, text/javascript, */*; q=0.01", 'Accept-Language'="en-US,en;q=0.5", 'Accept-Encoding'="gzip, deflate", 'Content-Type'="application/x-www-form-urlencoded; charset=UTF-8", 'X-Requested-With'="XMLHttpRequest", 'Referer'=paste("\"http://", url, "\"", sep="")),
 									style="post")
 				# progress is being made in the loop
 				pb <- txtProgressBar(min=1, max=length(x), style=3) #style=3 would allow to see the percent and no new lines would be added while it loops through
 				setTxtProgressBar(pb, i)
-				Sys.sleep(0.5) #put the system to sleep after each URL is being fetched
+				Sys.sleep(0.1) #put the system to sleep after each URL is being fetched
 				# perform a system clean-up by removing the user name and password from the vector
 				# once all of the variables have been counted for
 				if(i==length(x)){
@@ -169,15 +169,15 @@ getPI <- function(url, x, dump, ssl=FALSE){
 		options(RCurlOptions = list(cainfo = system.file("CurlSSL", "cacert.pem", package = "RCurl")))
 		if(url.exists(url)){
 			for (i in 1:length(x)){
-				dump[i] <- postForm(url, 
-									text = x[i], 
+				dump[i] <- postForm(url,
+									text = x[i],
 									button = "Analyze",
-									httpheader=c('Host'=paste("\"", url, "\"", sep=""), 'User-Agent'="Mozilla/5.0 (Windows NT 6.1; WOW64; rv:31.0) Gecko/20100101 Firefox/31.0", 'Accept'="application/json, text/javascript, */*; q=0.01", 'Accept-Language'="en-US,en;q=0.5", 'Accept-Encoding'="gzip, deflate", 'Content-Type'="application/x-www-form-urlencoded; charset=UTF-8", 'X-Requested-With'="XMLHttpRequest", 'Referer'=paste("\"https://", url, "\"", sep="")), 
+									httpheader=c('Host'=paste("\"", url, "\"", sep=""), 'User-Agent'="Mozilla/5.0 (Windows NT 6.1; WOW64; rv:31.0) Gecko/20100101 Firefox/31.0", 'Accept'="application/json, text/javascript, */*; q=0.01", 'Accept-Language'="en-US,en;q=0.5", 'Accept-Encoding'="gzip, deflate", 'Content-Type'="application/x-www-form-urlencoded; charset=UTF-8", 'X-Requested-With'="XMLHttpRequest", 'Referer'=paste("\"https://", url, "\"", sep="")),
 									style="post")
 				# progress is being made in the loop
 				pb <- txtProgressBar(min=1, max=length(x), style=3) #style=3 would allow to see the percent and no new lines would be added while it loops through
 				setTxtProgressBar(pb, i)
-				Sys.sleep(0.5) #put the system to sleep after each URL is being fetched
+				Sys.sleep(0.1) #put the system to sleep after each URL is being fetched
 				# perform a system clean-up by removing the user name and password from the vector
 				# once all of the variables have been counted for
 				if(i==length(x)){
@@ -190,7 +190,7 @@ getPI <- function(url, x, dump, ssl=FALSE){
 			cat("Error in URL since it doesn't exist\n")
 		}
 	}
-	
+
 	return(dump)
 } #dump
 
@@ -209,7 +209,7 @@ getPI <- function(url, x, dump, ssl=FALSE){
 #' new.x4 <- getPI2(url="https://www.example.com", x=data$field, dump=NULL, win=TRUE, capath="C:/pathtoca/cacert.pem") # store results into new var x4 and using windows
 getPI2 <- function(url, x, usr, pwd, dump, ssl=FALSE, win=FALSE, capath){
 	dump = NULL;
-	
+
 	if(ssl==FALSE & substring(url, 0, 5)=="https"){
 		cat("\n This is an encrypted connection, but you did not identify by using ssl=TRUE on function. Will continue insecurely. \n")
 		for (i in 1:length(x)){
@@ -222,7 +222,7 @@ getPI2 <- function(url, x, usr, pwd, dump, ssl=FALSE, win=FALSE, capath){
 			# progress is being made in the loop
 			pb <- txtProgressBar(min=1, max=length(x), style=3) #style=3 would allow to see the percent and no new lines would be added while it loops through
 			setTxtProgressBar(pb, i)
-			Sys.sleep(0.5) #put the system to sleep after each URL is being fetched
+			Sys.sleep(0.1) #put the system to sleep after each URL is being fetched
 			# perform a system clean-up by removing the user name and password from the vector
 			# once all of the variables have been counted for
 			if(i==length(x)){
@@ -242,11 +242,11 @@ getPI2 <- function(url, x, usr, pwd, dump, ssl=FALSE, win=FALSE, capath){
 			if(win==FALSE){
 				dump[i] <- system(paste('curl -s -X POST -u ', usr, ':', pwd, ' -H "Content-Type: text/plain" -d "', x[i], '" ', url, sep=""), intern=TRUE) # send command to curl securely. Do not record progress.
 			}
-			
+
 			# progress is being made in the loop
 			pb <- txtProgressBar(min=1, max=length(x), style=3) #style=3 would allow to see the percent and no new lines would be added while it loops through
 			setTxtProgressBar(pb, i)
-			Sys.sleep(0.5) #put the system to sleep after each URL is being fetched
+			Sys.sleep(0.1) #put the system to sleep after each URL is being fetched
 			# perform a system clean-up by removing the user name and password from the vector
 			# once all of the variables have been counted for
 			if(i==length(x)){
@@ -255,13 +255,13 @@ getPI2 <- function(url, x, usr, pwd, dump, ssl=FALSE, win=FALSE, capath){
 			}
 		}
 	}
-	
+
 	return(dump)
 } #dump
 
 #' Format JSON field
 #'
-#' This function will format JSON field by making it uniform and removing "word_count_message".  This is a result form PI if the word count is too low.  It will also add the person name from the data frame.
+#' This function will format JSON field by making it uniform and removing "warnings" and "word_count_message".  This is a result form PI if the word count is too low.  It will also add the person name from the data frame.
 #' @param x would be the field that wanted to be formatted.
 #' @keywords json format clean
 #' @export
@@ -269,11 +269,12 @@ getPI2 <- function(url, x, usr, pwd, dump, ssl=FALSE, win=FALSE, capath){
 #' clean.json <- fmtJSON(x, pname="Joe Doe") # Format JSON by adding person name
 fmtJSON <- function(x, pname){
 	# do some data clean up to make the JSON uniform
+	x <- gsub(",\"warnings\".*]", "", x) # find and replace warnings towards end of json string 
 	x <- gsub(",\"word_count_message\":\"There were \\d+.* words in the input. We need a minimum of 3,500, preferably 6,000 or more, to compute statistically significant estimates\"", "", x) # find and replace if record contains word_count_message and replace it with nothing
-	
+
 	# only select the dataset after the first { bracket }"
 	x <- paste("{\"person\":\"", pname, "\",", substring(x, 2), sep="") # add person profile prior of output
-	
+
 	return(x)
 }
 
@@ -291,14 +292,14 @@ exportPI <- function(data, json, output){
 				"Neuroticism", "Adventurousness", "Artistic interests", "Emotionality",
 				"Imagination", "Intellect", "Liberalism", "Achievement striving", "Cautiousness",
 				"Dutifulness", "Orderliness", "Self-discipline", "Self-efficacy", "Activity level",
-				"Assertiveness", "Cheerfulness", "Excitement-seeking", 
-				"Friendliness", "Gregariousness", "Altruism", "Cooperation", "Modesty", 
-				"Morality", "Sympathy", "Trust", "Anger", "Anxiety", "Depression", 
+				"Assertiveness", "Cheerfulness", "Excitement-seeking",
+				"Friendliness", "Gregariousness", "Altruism", "Cooperation", "Modesty",
+				"Morality", "Sympathy", "Trust", "Anger", "Anxiety", "Depression",
 				"Immoderation", "Self-consciousness", "Vulnerability", "Challenge", "Closeness", "Curiosity",
 				"Excitement", "Harmony", "Ideal", "Liberty", "Love", "Practicality",
 				"Self-expression", "Stability", "Structure", "Conservation", "Openness to change",
 				"Hedonism", "Self-enhancement", "Self-transcendence")
-	
+
 	#loop through each json string
 	#reused on Oct 10, 2015 -> https://github.com/IBMPredictiveAnalytics/Watson-Personality-Insights/blob/master/Source%20code/script.r
 	for(q in 1:length(json)){
@@ -306,8 +307,8 @@ exportPI <- function(data, json, output){
 
 		if(length(res)==2){
 			dump <- rep(NA,52)
-		} 
-		else{ 
+		}
+		else{
 			wc <- res$word_count #get word count variable
 			res <- res$tree$children #get main variables from PI results
 			dump <- NULL
@@ -327,10 +328,10 @@ exportPI <- function(data, json, output){
 		t <- as.data.frame(c(wc, t)) #attach word count var to df
 		dd <- cbind(data[q, ], t) #select current json loop and attach calc results
 		colnames(dd) <- c(colnames(data), "word_count", pinames) #rename columns to proper names
-		
+
 		#do output
 		if(q==1){
-			write.table(dd, paste(output, ".csv", sep=""), sep=",", row.names=FALSE, append=FALSE) #export the data on a csv format	
+			write.table(dd, paste(output, ".csv", sep=""), sep=",", row.names=FALSE, append=FALSE) #export the data on a csv format
 		}
 		else{
 			write.table(dd, paste(output, ".csv", sep=""), sep=",", row.names=FALSE, append=TRUE, col.names=FALSE) #append data and don't include column names
--- README.md
+++ README.md
@@ -1,56 +1,26 @@
 # rWatsonPI
-=================
-Load unstructured data to Watson Personality Insights (PI) via post Multipart/Form-data by interacting with PI API.  Such an interaction will be posted on JSON format and then stored back into R on the same format as well.  User will have the capabilities of reformatting the JSON file into a CSV and exporting such results for analysis.
-=================
+Load unstructured data to Watson Personality Insights (PI) by interacting with the API or via post Multipart/Form-data.  Such an interaction will be posted on JSON format and then stored back into R on the same format as well.  User will have the capabilities of reformatting the JSON file into a CSV and exporting such results for analysis.
 
 ## Personality Insights Nodejs Starter Application
-=================
 The IBM Watson [Personality Insights](https://github.com/watson-developer-cloud/personality-insights-nodejs) service uses linguistic analysis to extract cognitive and social characteristics from input text such as email, text messages, tweets, forum posts, and more. By deriving cognitive and social preferences, the service helps users to understand, connect to, and communicate with other people on a more personalized level.
 
 ## Getting Started
-=================
+
 ### Install and activate package in R
 1. From R console download and install package from GitHub
 ``` R
-	install.packages("devtools")
-	devtools::install_github("blacknred0/rwatsonpi")
+install.packages("devtools")
+devtools::install_github("blacknred0/rwatsonpi")
 ```
 2. Activate rWatsonPI package
 ``` R
-    library("rwatsonpi") #activate library
+library("rwatsonpi") #activate library
 ```
 
-### Working with remote environment
-1. It is assumed that you already have some sort of BlueMix environment that you can use.  If you do not have one, you will need to have one created/configured.
-2. Make sure that you activate the package and necessary libraries
-3. Run main script
-``` R
-    df <- read.table("sample.csv", sep=",", quote = "\"", header=TRUE, fill=FALSE) #import sample csv into R
-    df$text <- clnTxt(df$text) #clean records and replace field
-	selMeaningfulRecs(df, df$text) #select meaningful records based on criteria
-	fetch <- getPI("https://www.example.com", df.sel$text, ssl=TRUE) #store PI JSON results. transfer will be encrypted. if you do not want encryption, simply remove ssl
-    fj <- fmtJSON(fetch, df.sel$person) #attach identifier to results
-	exportPI(fj) #export to csv
-```
+## Run rWatsonPI using cURL
+---
 
-### Working with local environment
-1. It is assumed that you already have some sort of BlueMix environment that you can use.  If you do not have one, you will need to have one created/configured.
-2. Make sure that you activate the package and necessary libraries
-3. Run main script
-``` R
-    df <- read.table("sample.csv", sep=",", quote = "\"", header=TRUE, fill=FALSE) #import sample csv into R
-    df$text <- clnTxt(df$text) #clean records and replace field
-	selMeaningfulRecs(df, df$text) #select meaningful records based on criteria
-    startPI("C:/watson-developer-cloudpersonality-insights-nodejs") #location where personality insights is located
-    fetch <- getPI("http://localhost:3000", df.sel$text) #store PI JSON results.
-    fj <- fmtJSON(fetch, df.sel$person) #attach identifier to results
-	exportPI(fj) #export to csv
-    stopPI() 
-```
-
-# rWatsonPI meets CURL natively instead of form
-=================
-## Install and setup
+### Install and setup
 If you want to use curl natively on Linux or Mac, you are set.  Now, if you want to use it on Windows, there is some configuration that needs to take place.
 1. Download curl zip
 2. Extract all file and folders
@@ -66,51 +36,86 @@ If you want to use curl natively on Linux or Mac, you are set.  Now, if you want
   8. Add a semicolon followed by the path to where you curl.exe is located (e.g. ;C:\curl)
 Now you can run from the command line by typing:
 ```
-	curl www.google.com
+curl www.google.com
 ```
 
-## Configure to work with SSL
+### Configure to work with SSL
 You do not need to do this unless you want to use SSL.  You do have the option to send data unencrypted.  
 
 Here are the steps to get it configured.
-1. Go to [CA cert page](http://curl.haxx.se/docs/caextract.html) 
+1. Go to [CA cert page](http://curl.haxx.se/docs/caextract.html)
 2. Download "cacert.pem" by saving into your computer
 3. Move "cacert.pem" into "C:\curl" folder
 
 Now that you have the environment configured, then you should be able to use getPI2() function from rWatsonPI.
 For more information on how to work with SSL certificates here is the [direct link](http://curl.haxx.se/docs/sslcerts.html) to the documentation.
 
-## Working with local environment
-1. It is assumed that you already have some sort of BlueMix environment that you can use.  If you do not have one, you will need to have one created/configured.
+### Working with remote environment
+Since there have been changes to Watson Personality Insights, this process/functions will be the default going forward.
+
+1. It is assumed that you already have some sort of BlueMix environment that you can use.  If you do not have one, you will need to have one created/configured. Also, if you are on Windows you will need to configure [cURL](#Run-rWatsonPI-using-cURL)
 2. [Make sure that you activate the package and necessary libraries](#Install-and-activate-package-in-R)
 3. Run main script
 ``` R
-    df <- read.table("sample.csv", sep=",", quote = "\"", header=TRUE, fill=FALSE) #import sample csv into R
-    df$text <- clnTxt(df$text) #clean records and replace field
-	selMeaningfulRecs(df, df$text) #select meaningful records based on criteria
-    startPI("C:/watson-developer-cloudpersonality-insights-nodejs") #location where personality insights is located
-    fetch <- getPI2("https://gateway.watsonplatform.net/personality-insights/api/v2/profile", df.sel$text, usr="bunch-of-ugly-letters-and-numbers-from-blue-mix", pwd="something-really-hard-to-remember") #store PI JSON results.
-    fj <- fmtJSON(fetch, df.sel$person) #attach identifier to results
-	exportPI(fj) #export to csv
-    stopPI() 
+df <- read.table("sample.csv", sep=",", quote = "\"", header=TRUE, fill=FALSE) #import sample csv into R
+df$text <- clnTxt(df$text) #clean records and replace field
+selMeaningfulRecs(df, df$text) #select meaningful records based on criteria
+fetch <- getPI2("https://gateway.watsonplatform.net/personality-insights/api/v2/profile", df.sel$text, usr="bunch-of-ugly-letters-and-numbers-from-blue-mix", pwd="something-really-hard-to-remember") #store PI JSON results
+fj <- fmtJSON(fetch, df.sel$person) #attach identifier to results
+df.sel.t <- data.frame(person=df.sel$person)
+exportPI(df.sel.t, fj, "nameofcsv") #export to csv
+```
+
+## Run rWatsonPI using forms
+---
+
+Prior of new implementation of Watson Personality Insights, you were able to use forms.  Now, if you would still like to use forms to post and retrieve your data, you might want to go to  [this commit](https://github.com/watson-developer-cloud/personality-insights-nodejs/commit/8cdaeaf3a9b1f5d0f9222432bcace3fd0110252a).
+
+## Working with local environment
+1. It is assumed that you already have some sort of BlueMix environment that you can use.  If you do not have one, you will need to have one created/configured.
+2. Make sure that you activate the package and necessary libraries
+3. Run main script
+``` R
+df <- read.table("sample.csv", sep=",", quote = "\"", header=TRUE, fill=FALSE) #import sample csv into R
+df$text <- clnTxt(df$text) #clean records and replace field
+selMeaningfulRecs(df, df$text) #select meaningful records based on criteria
+startPI("C:/watson-developer-cloudpersonality-insights-nodejs") #location where personality insights is located
+fetch <- getPI("http://localhost:3000", df.sel$text) #store PI JSON results. For Windows, add parameter "win=TRUE"
+fj <- fmtJSON(fetch, df.sel$person) #attach identifier to results
+df.sel.t <- data.frame(person=df.sel$person)
+exportPI(df.sel.t, fj, "nameofcsv") #export to csv
+stopPI()
+```
+
+## Working with remote environment
+1. It is assumed that you already have some sort of BlueMix environment that you can use.  If you do not have one, you will need to have one created/configured.
+2. Make sure that you activate the package and necessary libraries
+3. Run main script
+``` R
+df <- read.table("sample.csv", sep=",", quote = "\"", header=TRUE, fill=FALSE) #import sample csv into R
+df$text <- clnTxt(df$text) #clean records and replace field
+selMeaningfulRecs(df, df$text) #select meaningful records based on criteria
+fetch <- getPI("https://www.example.com", df.sel$text, ssl=TRUE) #store PI JSON results. transfer will be encrypted. if you do not want encryption, simply remove ssl. For Windows, add parameter "win=TRUE"
+fj <- fmtJSON(fetch, df.sel$person) #attach identifier to results
+df.sel.t <- data.frame(person=df.sel$person)
+exportPI(df.sel.t, fj, "nameofcsv") #export to csv
 ```
 
 # Issues
-=================
+
 * If for some reason the package is not activating properly, you can force the download and activation of all required packages by invoking the following function.
 ``` R
-	pkgHC() #download and install R packages
+pkgHC() #download and install R packages
 ```
-* When using ``` gePI() ``` function in R you need to make sure that you increase the number of records that could be processed at time on [Personality Insights](https://github.com/watson-developer-cloud/personality-insights-nodejs/blob/master/config/rate-limit.js#L30).  Otherwise reCAPTCHA will kick in and you won't be able to run more than 4 records at time. 
 
 # How to contribute
-=================
+
 See [TODO](TODO) for list of enhancements that could be made.
 
 # License
-=================
+
 This sample code is licensed under Apache 2.0. Full license text is available in [LICENSE](LICENSE).
 
 # Open Source @ IBM
-=================
+
 Find more open source projects on the [IBM Github Page](http://ibm.github.io/)
